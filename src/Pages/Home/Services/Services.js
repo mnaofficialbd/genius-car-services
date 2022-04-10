ac7b7db5157e496ec2d0fd24c6134d837c813ff4
mnaofficial <mna.official92@gmail.com>
2022-04-11 00:49:24 +0600

    all file add

---
+++ src/Pages/Home/Services/Services.js
@@ -0,0 +1,23 @@
+import React, { useEffect, useState } from 'react';
+import "./Services.css"
+
+
+
+const Services = () => {
+
+    const [services,setServices] = useState([]);
+
+    useEffect(() => {
+        fetch('services.json')
+            .then(res=>res.json())
+            .then(data=>setServices(data))
+    }, [])
+
+    return (
+        <div>
+            <h2>Services</h2>
+        </div>
+    );
+};
+
+export default Services;
---
+++ src/Pages/Shared/Footer/Footer.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import "./Footer.css"
+
+const Footer = () => {
+    return (
+        <footer>
+            <p><small>copyright @ /dynamic year/</small></p>
+        </footer>
+    );
+};
+
+export default Footer;
